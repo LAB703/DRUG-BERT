6768de6ef76072b82e66acf0d86f8df526c45236
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-15 20:22:52 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -1,6 +1,15 @@
 import streamlit as st
 import json
 import torch
+from transformers import BertTokenizerFast
+from model import BertForTokenAndSequenceJointClassification
 
-
-st.write('')
+@st.cache(allow_output_mutation=True)
+def load_model():
+    tokenizer = BertTokenizerFast.from_pretrained('bert-base-cased')
+    model = BertForTokenAndSequenceJointClassification.from_pretrained(
+            "QCRI/PropagandaTechniquesAnalysis-en-BERT",
+             revision="v0.1.0")
+    return tokenizer, model
+    
+tokenizer, model = load_model()
