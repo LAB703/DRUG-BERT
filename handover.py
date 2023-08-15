91b50985c9cf762dc8ce425cc98f3879d4cee95f
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-15 19:06:41 +0900

    Update handover.py

--- handover.py
+++ handover.py
@@ -75,6 +75,10 @@ div.viewerBadge_link__1S137 {
 div.css-j7qwjs.e1fqkh3o5 {
     font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', sans-serif;
 }
+
+div.css-q16mip e3g6aar0 {
+    display:none:
+}
 a.viewerBadge_container__1QSob {
     z-index: 50;
     font-size: .875rem;
