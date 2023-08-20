5ba1cc77fa97edc72956c5be35f58ac3271f9e6a
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-20 19:29:31 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -6,14 +6,12 @@ from transformers import BertTokenizerFast, BertForSequenceClassification
 @st.cache(allow_output_mutation=True)
 def load_model(model_name_or_path):
     tokenizer = BertTokenizerFast.from_pretrained(model_name_or_path)
-    model = BertForSequenceClassification.from_pretrained(
-            "QCRI/PropagandaTechniquesAnalysis-en-BERT",
-             revision="v0.1.0")
+    model = BertForSequenceClassification.from_pretrained(model_name_or_path)
     return tokenizer, model
     
 tokenizer, model = load_model('skt/kobert-base-v1')
 
 text="누구든지 아동·청소년이용음란물임을 알면서 이를 소지하여서는 아니된다."
-st.wirte(text)
+st.write(text)
 tokenized_text = tokenizer.tokenize(text)
 st.write('tokenized_text >>' + tokenized_text)
