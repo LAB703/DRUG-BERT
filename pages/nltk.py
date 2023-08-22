a6006f3942abe141a8258082aad160ab031d95a0
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-22 22:27:04 +0900

    Update nltk.py

--- pages/nltk.py
+++ pages/nltk.py
@@ -1,4 +1,27 @@
 import streamlit as st
+import json
+import torch
+from transformers import BertTokenizerFast, BertForSequenceClassification
+
+import style
+st.markdown(style.style, unsafe_allow_html=True)
+    
+@st.cache(allow_output_mutation=True)
+    
+def load_model(model_name_or_path):
+    tokenizer = BertTokenizerFast.from_pretrained(model_name_or_path)
+    model = BertForSequenceClassification.from_pretrained(model_name_or_path)
+    return tokenizer, model
+    
+# tokenizer, model = load_model("bert-base-multilingual-cased")
+tokenizer, model = load_model("beomi/kcbert-base")
+text="누구든지 아동·청소년이용음란물임을 알면서 이를 소지하여서는 아니된다."
+st.write(text)
+tokenized_text = tokenizer.tokenize(text)
+st.write('tokenized_text >>', tokenized_text)
+
+
+import streamlit as st
 import regex as re
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
