4e0a4f9253f780d7cc7ffb63ac841af9b7297bb5
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-22 20:13:17 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -4,8 +4,6 @@ import torch
 from transformers import BertTokenizerFast, BertForSequenceClassification
 
 import style
-
-
 st.markdown(style.style, unsafe_allow_html=True)
     
 @st.cache(allow_output_mutation=True)
@@ -15,8 +13,8 @@ def load_model(model_name_or_path):
     model = BertForSequenceClassification.from_pretrained(model_name_or_path)
     return tokenizer, model
     
-tokenizer, model = load_model("bert-base-multilingual-cased")
-
+# tokenizer, model = load_model("bert-base-multilingual-cased")
+tokenizer, model = load_model("beomi/kcbert-base")
 text="누구든지 아동·청소년이용음란물임을 알면서 이를 소지하여서는 아니된다."
 st.write(text)
 tokenized_text = tokenizer.tokenize(text)
