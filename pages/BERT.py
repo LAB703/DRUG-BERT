1b0f5111c969234f01cbb02cc7dc178084ebeae2
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-22 21:41:11 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -80,22 +80,9 @@ if submitted:
     # SDG labels list
 
     label_list = [
-        'GOAL 1: No Poverty',
-        'GOAL 2: Zero Hunger',
-        'GOAL 3: Good Health and Well-being',
-        'GOAL 4: Quality Education',
-        'GOAL 5: Gender Equality',
-        'GOAL 6: Clean Water and Sanitation',
-        'GOAL 7: Affordable and Clean Energy',
-        'GOAL 8: Decent Work and Economic Growth',
-        'GOAL 9: Industry, Innovation and Infrastructure',
-        'GOAL 10: Reduced Inequality',
-        'GOAL 11: Sustainable Cities and Communities',
-        'GOAL 12: Responsible Consumption and Production',
-        'GOAL 13: Climate Action',
-        'GOAL 14: Life Below Water',
-        'GOAL 15: Life on Land',
-        'GOAL 16: Peace, Justice and Strong Institutions'
+        '🚨⛔️🚫🔴: 마약광고',
+        '✅🟡🟢: 마약광고 의심글',
+        '🔵: 마약글 비해당'
     ]
 
     if Text_entry == "":
