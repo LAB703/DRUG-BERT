a84d0b4a1352e2020e9e2486273a702234ac7884
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 19:45:09 +0900

    Update header.py

--- header.py
+++ header.py
@@ -1,11 +1,13 @@
 import streamlit as st 
 
-t1, t2, t3 = st.columns((2,5,3)) 
-# Korean_National_Police_Agency_Emblem
-t1.image('images/index.png', width = 120)
-t2.header('SNS 마약 거래 모니터링 시스템')
-t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M:%S"))
-st.write('')
-st.write('')
-st.write('')
-st.write('---')
+
+def header() :
+  t1, t2, t3 = st.columns((2,5,3)) 
+  # Korean_National_Police_Agency_Emblem
+  t1.image('images/index.png', width = 120)
+  t2.header('SNS 마약 거래 모니터링 시스템')
+  t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M:%S"))
+  st.write('')
+  st.write('')
+  st.write('')
+  st.write('---')
