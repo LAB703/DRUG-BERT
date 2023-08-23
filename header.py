7b391217833af2f59ccfbfbf477f89b6fc5e5dfa
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 19:56:56 +0900

    Update header.py

--- header.py
+++ header.py
@@ -2,7 +2,27 @@ import streamlit as st
 from datetime import timedelta, datetime
 from pytz import timezone
 
+weekday_dict = {
+    0: '월요일',
+    1: '화요일',
+    2: '수요일',
+    3: '목요일',
+    4: '금요일',
+    5: '토요일',
+    6: '일요일'
+}
 
+ampm_dict = {
+    'AM': '오전',
+    'PM': '오후'
+}
+
+tz = timezone('Asia/Seoul')
+
+def formatting(time_now) :
+  formatted_time = time_now.strftime("%Y년 %m월 %d일") + ' ' + weekday_dict[time_now.weekday()] + ' ' + ampm_dict[time_now.strftime("%p")] + ' ' + time_now.strftime("%I:%M")
+  return formatted_time
+  
 def header() :
   t1, t2, t3 = st.columns((2,5,3)) 
   # Korean_National_Police_Agency_Emblem
@@ -11,11 +31,11 @@ def header() :
   re_run = t3.button('🔄 모델 갱신')
   if re_run :
       st.experimental_rerun()
-      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M")
+      last_update = formatting(datetime.now(tz))
   else : 
-      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:00")
+      last_update = formatting(datetime.now(tz))
   
-  t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M"))
+  t3.write(datetime.now(tz)
   t3.write('마지막 갱신 : ' + last_update)
   st.write('')
   st.write('---')
