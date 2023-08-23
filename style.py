9aa4970f329e6771ff743396185fba1635e79fac
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 15:08:07 +0900

    Update style.py

--- style.py
+++ style.py
@@ -81,7 +81,7 @@ div.streamlit-expanderHeader.st-ae.st-bq.st-ag.st-ah.st-ai.st-aj.st-br.st-bs.st-
 textbox_style = """
     <style>
     .stTextArea [data-baseweb=base-input] {
-        background-image: linear-gradient(140deg, rgb(54, 36, 31) 0%, rgb(121, 56, 100) 50%, rgb(106, 117, 25) 75%);
+        background-color: #ffca00;
         -webkit-text-fill-color: white;
     }
 
@@ -92,24 +92,4 @@ textbox_style = """
     </style>
     """
 
-# st.markdown("""
-#     <style>
-#     .stTextArea [data-baseweb=base-input] {
-#         background-image: linear-gradient(140deg, rgb(54, 36, 31) 0%, rgb(121, 56, 100) 50%, rgb(106, 117, 25) 75%);
-#         -webkit-text-fill-color: white;
-#     }
 
-#     .stTextArea [data-baseweb=base-input] [disabled=""]{
-#         background-image: linear-gradient(45deg, red, purple, red);
-#         -webkit-text-fill-color: gray;
-#     }
-#     </style>
-#     """,unsafe_allow_html=True)
-
-# disable_textarea = st.checkbox("Disable text area:")
-
-# st.text_area(
-#     label="Text area:",
-#     value="This is a repeated sentence "*20,
-#     height=300,
-#     disabled=disable_textarea)
