793f3d5f01fb258ce0a2a07970702f2d1ebf9086
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 19:48:28 +0900

    Update header.py

--- header.py
+++ header.py
@@ -4,12 +4,18 @@ from pytz import timezone
 
 
 def header() :
-  t1, t2, t3 = st.columns((2,4,2)) 
+  t1, t2, t3 = st.columns((2,5,3)) 
   # Korean_National_Police_Agency_Emblem
   t1.image('images/index.png', width = 120)
   t2.header('SNS 마약 거래 모니터링 시스템')
-  t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M:%S"))
-  st.write('')
-  st.write('')
+  re_run = t3.button('🔄 모델 갱신')
+  if re_run :
+      st.experimental_rerun()
+      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M")
+  else : 
+      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:00")
+  
+  t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M"))
+  t3.write('마지막 갱신 : ' + last_update)
   st.write('')
   st.write('---')
