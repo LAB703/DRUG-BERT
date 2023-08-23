855d9f498fdc8738ed0ab1fbf06d5f127034b513
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 21:17:05 +0900

    Update title.py

--- title.py
+++ title.py
@@ -3,7 +3,7 @@ from datetime import timedelta, datetime
 from pytz import timezone
 
 def header() :
-  t1, t2, t3 = st.columns((2,5,3)) 
+  t1, t2, t3 = st.columns((3,5,3)) 
   # Korean_National_Police_Agency_Emblem
   t1.image('images/index.png', width = 120)
   t2.header('SNS 마약 거래 모니터링 시스템')
