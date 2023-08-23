5cfe7b24b737fa0dcaf0febfb7dad96dc1daf861
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 15:05:52 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -21,16 +21,7 @@ st.set_page_config(page_title="Drug Classifier", layout= "wide", initial_sidebar
 
 import style
 st.markdown(style.style, unsafe_allow_html=True)
-
 st.markdown(style.textbox_style,unsafe_allow_html=True)
-
-disable_textarea = st.checkbox("Disable text area:")
-
-st.text_area(
-    label="Text area:",
-    value="This is a repeated sentence "*20,
-    height=300,
-    disabled=disable_textarea)
 ########################################################################################
 
 t1, t2, t3 = st.columns((2,5,3)) 
