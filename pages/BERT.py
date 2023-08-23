a0bd2842730220a796629da4274d46b45d4f5764
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 14:58:22 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -21,6 +21,8 @@ st.set_page_config(page_title="Drug Classifier", layout= "wide", initial_sidebar
 
 import style
 st.markdown(style.style, unsafe_allow_html=True)
+st.markdown(style.textbox_style, unsafe_allow_html=True)
+
 ########################################################################################
 
 t1, t2, t3 = st.columns((2,5,3)) 
