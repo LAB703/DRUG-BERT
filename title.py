09d1d421d9b16accfb04ec4264c271cddd714d57
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 20:06:41 +0900

    Update title.py

--- title.py
+++ title.py
@@ -26,14 +26,15 @@ def header() :
   # Korean_National_Police_Agency_Emblem
   t1.image('images/index.png', width = 120)
   t2.header('SNS 마약 거래 모니터링 시스템')
+  tz = timezone('Asia/Seoul')
   re_run = t3.button('🔄 모델 갱신')
   if re_run :
       st.experimental_rerun()
-      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M")
+      last_update = datetime.now(tz).strftime("%Y-%m-%d %A %H:%M")
   else : 
-      last_update = datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:00")
+      last_update = datetime.now(tz).strftime("%Y-%m-%d %A %H:00")
   
-  t3.write(datetime.now(timezone('Asia/Seoul')).strftime("%Y-%m-%d %A %H:%M"))
+  t3.write(datetime.now(tz).strftime("%Y-%m-%d %A %H:%M"))
   t3.write('마지막 갱신 : ' + last_update)
   st.write('')
   st.write('')
