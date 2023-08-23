165cf32f75e4b747c6442e399ad164517cf66993
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 20:09:33 +0900

    Update title.py

--- title.py
+++ title.py
@@ -4,22 +4,18 @@ from pytz import timezone
 
 def formatting(time_now) :
     weekday_dict = {
-    0: '월요일',
-    1: '화요일',
-    2: '수요일',
-    3: '목요일',
-    4: '금요일',
-    5: '토요일',
-    6: '일요일'
+    0: '(월)',
+    1: '(화)',
+    2: '(수)',
+    3: '(목)',
+    4: '(금)',
+    5: '(토)',
+    6: '(일)'
     }
 
-    ampm_dict = {
-        'AM': '오전',
-        'PM': '오후'
-    }
     tz = timezone('Asia/Seoul')
-      formatted_time = time_now.strftime("%Y년 %m월 %d일") + ' ' + weekday_dict[time_now.weekday()] + ' ' + ampm_dict[time_now.strftime("%p")] + ' ' + time_now.strftime("%I:%M")
-      return formatted_time
+    formatted_time = time_now.strftime("%Y년 %m월 %d일") #+ ' ' + weekday_dict[time_now.weekday()] + ' ' time_now.strftime("%I:%M")
+    return formatted_time
 
 def header() :
   t1, t2, t3 = st.columns((2,5,3)) 
