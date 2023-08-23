3ed542092b2c216e1e6a17b8fc3bbc9336bf5673
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 21:41:57 +0900

    Create chatGPT.py

---
+++ pages/chatGPT.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import title
+
+title.header()
+# https://ask-my-pdf.streamlit.app/
+#https://gpt4autocoder.streamlit.app/
+# https://knowledgegpt.streamlit.app/
+# https://langchain-chat-search.streamlit.app/
+# https://langchain-quickstart.streamlit.app
