ed835fc6a6eef5debea031f7baa594a7dd478834
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-23 14:55:03 +0900

    Update BERT.py

--- pages/BERT.py
+++ pages/BERT.py
@@ -83,6 +83,28 @@ def prep_text(text):
 
 st.header("🚦 마약 글 분류")
 
+st.markdown("""
+    <style>
+    .stTextArea [data-baseweb=base-input] {
+        background-image: linear-gradient(140deg, rgb(54, 36, 31) 0%, rgb(121, 56, 100) 50%, rgb(106, 117, 25) 75%);
+        -webkit-text-fill-color: white;
+    }
+
+    .stTextArea [data-baseweb=base-input] [disabled=""]{
+        background-image: linear-gradient(45deg, red, purple, red);
+        -webkit-text-fill-color: gray;
+    }
+    </style>
+    """,unsafe_allow_html=True)
+
+disable_textarea = st.checkbox("Disable text area:")
+
+st.text_area(
+    label="Text area:",
+    value="This is a repeated sentence "*20,
+    height=300,
+    disabled=disable_textarea)
+
 
 # Form to recieve input text ### 여기를 리셋형태로 바꿀 거임 
 st.markdown("##### Text Input")
