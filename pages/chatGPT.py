e4befcf4e669ea4682a825b98193cf5c59c109b1
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-26 02:52:21 +0900

    Update chatGPT.py

--- pages/chatGPT.py
+++ pages/chatGPT.py
@@ -28,3 +28,20 @@ uploaded_file = st.file_uploader(
 
 if not uploaded_file:
     st.stop()
+
+@st.cache_data(show_spinner=False)
+def is_open_ai_key_valid(openai_api_key) -> bool:
+    if not openai_api_key:
+        st.error("Please enter your OpenAI API key in the sidebar!")
+        return False
+    try:
+        openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": "test"}],
+            api_key=openai_api_key,
+        )
+    except Exception as e:
+        st.error(f"{e.__class__.__name__}: {e}")
+        logger.error(f"{e.__class__.__name__}: {e}")
+        return False
+    return True
