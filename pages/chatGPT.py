7476eb851404aa77da480e74d31d5eb0a481492f
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-26 03:02:05 +0900

    Update chatGPT.py

--- pages/chatGPT.py
+++ pages/chatGPT.py
@@ -22,8 +22,9 @@ with st.sidebar:
             type="password",
             placeholder="Paste your OpenAI API key here (sk-...)",
             help="You can get your API key from https://platform.openai.com/account/api-keys.",  # noqa: E501
-            value=os.environ.get("OPENAI_API_KEY", None)
-            or st.session_state.get("OPENAI_API_KEY", ""),
+            value=st.session_state.get("OPENAI_API_KEY", ""),
+                # os.environ.get("OPENAI_API_KEY", None)  # local
+             
         )
 
         st.session_state["OPENAI_API_KEY"] = api_key_input
