8e3e2e2581dd61dfe52573fd70bdb380df15bd50
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-08-26 04:05:31 +0900

    Update chatGPT.py

--- pages/chatGPT.py
+++ pages/chatGPT.py
@@ -30,18 +30,19 @@ with st.sidebar:
         st.session_state["OPENAI_API_KEY"] = api_key_input
 
         st.markdown("---")
-        st.markdown("# About")
-        st.markdown(
-            "📖KnowledgeGPT allows you to ask questions about your "
-            "documents and get accurate answers with instant citations. "
-        )
-        st.markdown(
-            "This tool is a work in progress. "
-            "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
-            "with your feedback and suggestions💡"
-        )
-        st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
-        st.markdown("---")
+        # st.markdown("# About")
+        # st.markdown(
+        #     "📖KnowledgeGPT allows you to ask questions about your "
+        #     "documents and get accurate answers with instant citations. "
+        # )
+        # st.markdown(
+        #     "This tool is a work in progress. "
+        #     "You can contribute to the project on [GitHub](https://github.com/mmz-001/knowledge_gpt) "  # noqa: E501
+        #     "with your feedback and suggestions💡"
+        # )
+        # st.markdown("Made by [mmz_001](https://twitter.com/mm_sasmitha)")
+        # st.markdown("---")
+
 
 openai_api_key = st.session_state.get("OPENAI_API_KEY")
 
@@ -70,7 +71,8 @@ def is_open_ai_key_valid(openai_api_key) -> bool:
     try:
         openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            messages=[{"role": "user", "content": "test"}],
+            messag
+                es=[{"role": "user", "content": "test"}],
             api_key=openai_api_key,
         )
     except Exception as e:
