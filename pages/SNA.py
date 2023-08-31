70b2870f3474d417d3c89a21dfad13f209949f74
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-09-01 01:59:39 +0900

    Update SNA.py

--- pages/SNA.py
+++ pages/SNA.py
@@ -63,8 +63,7 @@ def app():
   st.title("Graph Example")
   # st.sidebar.title("Welcome")      
   query_type = st.sidebar.selectbox("Query Type: ", ["Inspirationals", "Marvel"]) # could add more stuff here later on or add other endpoints in the sidebar.
-  config = Config(height=600, width=700, nodeHighlightBehavior=True, highlightColor="#F7A7A6", directed=True, Color = "white",
-                  collapsible=True)
+  config = Config(height=600, width=700, nodeHighlightBehavior=True, highlightColor="#F7A7A6", directed=True, collapsible=True)
 
   if query_type=="Inspirationals":
     st.subheader("Inspirationals")
