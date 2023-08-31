008d4c94f36bff95c94b050d3d728397720b6cc5
LAB703 <141557138+LAB703@users.noreply.github.com>
2023-09-01 03:46:19 +0900

    Update DRUG-BERT.py

--- pages/DRUG-BERT.py
+++ pages/DRUG-BERT.py
@@ -127,7 +127,7 @@ if submitted:
     
             fig = px.bar(df2, y="Likelihood", x="SDG",
                           color = 'SDG',
-                          color_discrete_map = {label_list[1]: '#172b9c',label_list[0]: '#b82d1d'}) #, 'versicolor': 'rgb(0,0,255)'})
+                          color_discrete_map = {label_list[0]: '#172b9c',label_list[1]: '#b82d1d'}) #, 'versicolor': 'rgb(0,0,255)'})
 
                          # color_discrete_sequence= ['#b82d1d', '#172b9c']) #, orientation="h")
 
